b4f4e0fc111f11088f1feaf9f4edd2cc18bfdfed
Tammy Metz <tammy@tammymetz.com>
2019-11-09 09:41:39 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { Component } from 'react'
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    fetch('')
+    .then(response => response.json())
+    .then(data => {
+      
+    })
+  }
+}
+
+export default App
